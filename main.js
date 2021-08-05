9ba25271e9c0b3c0626854a6ff3c2322b3ca5a4c
RichardFi <627838766@qq.com>
2021-08-05 17:42:47 +1000

    Init

---
+++ main.js
@@ -0,0 +1,109 @@
+function createNav () {
+  let previous = ''
+  let current = 'ABOUT'
+
+  changePage()
+  changeNavItem()
+
+  document.querySelectorAll('.navbar__item').forEach(function (element) {
+    element.onclick = function (event) {
+      event.preventDefault()
+
+      let to = event.target.getAttribute('href')
+      push(to)
+    }
+  })
+
+  function push (to) {
+    previous = current
+    current = to
+
+    changePage()
+    changeNavItem()
+  }
+
+  function changePage () {
+    let className = 'page--active'
+    console.log(current)
+    if (previous) {
+      let previousPage = document.querySelector('#' + previous)
+      previousPage.classList.remove(className)
+      document.querySelector('.main').classList.remove('main--' + previous)
+    }
+    document.querySelector('.main').classList.add('main--' + current)
+
+    let page = document.querySelector('#' + current)
+    page.classList.add(className)
+  }
+
+  function changeNavItem () {
+    let className = 'navbar__item--active'
+
+    if (previous) {
+      let previousNavItem = document.querySelector('[href="' + previous + '"]')
+      previousNavItem.classList.remove(className)
+    }
+
+    let navItem = document.querySelector('[href="' + current + '"]')
+    navItem.classList.add(className)
+  }
+}
+
+function productFilter () {
+  let previous = ''
+  let current = 'product'
+
+  changeProduct()
+  changeFilter()
+  document.querySelectorAll('.filter').forEach(function (element) {
+    element.onclick = function (event) {
+      event.preventDefault()
+
+      let to = event.target.getAttribute('href')
+      push(to)
+    }
+  })
+
+  function push (to) {
+    previous = current
+    current = to
+
+    changeProduct()
+    changeFilter()
+  }
+
+  function changeProduct () {
+    let className = 'product--active'
+
+    if (previous) {
+      document.querySelectorAll('.' + previous).forEach(function (element) {
+        element.classList.remove(className)
+      })
+    }
+
+    document.querySelectorAll('.' + current).forEach(function (element) {
+      element.classList.add(className)
+    })
+    /*     console.log(page)
+    page.classList.add(className) */
+  }
+
+  function changeFilter () {
+    let className = 'filter--active'
+
+    if (previous) {
+      let previousNavItem = document.querySelector('[href="' + previous + '"]')
+      previousNavItem.classList.remove(className)
+    }
+
+    let navItem = document.querySelector('[href="' + current + '"]')
+    navItem.classList.add(className)
+  }
+}
+
+function main () {
+  createNav()
+  productFilter()
+}
+
+main()
